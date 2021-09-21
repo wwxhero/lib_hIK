70566c6543c4eb48c0c735e81c71060238d976c4
wanxwang <wanxin-wang@uiowa.edu>
2021-09-21 14:44:16 -0500

    ready for test without +d_theta

--- src/IK_QSegment.cpp
+++ src/IK_QSegment.cpp
@@ -4,3 +4,93 @@
 BEGIN_ENUM_STR(IK_QSegment, Type)
 	ENUM_ITEM(R_xyz)
 END_ENUM_STR(IK_QSegment, Type)
+
+IK_QSegment::IK_QSegment(Type a_type, int n_dof)
+	: m_scale((Real)1.0)
+	, m_bodies{NULL, NULL}
+	, m_joints{NULL, NULL}
+	, m_max_extension((Real)0)
+	, m_DoF_id(-1)
+	, c_type(a_type)
+	, c_num_DoF(n_dof)
+	, c_idxFrom(0)
+	, c_idxTo(1)
+{
+
+}
+
+IK_QSegment::~IK_QSegment()
+{
+
+}
+
+bool IK_QSegment::Initialize(CArtiBodyNode* from, CArtiBodyNode* to)
+{
+	m_bodies[c_idxFrom] = from;
+	m_bodies[c_idxTo] = to;
+	m_joints[c_idxFrom] = from->GetJoint();
+	m_joints[c_idxTo] = to->GetJoint();
+	Eigen::Vector3r start_w = GlobalStart();
+	Eigen::Vector3r end_w = GlobalEnd();
+	m_max_extension = (end_w - start_w).norm();
+	return m_max_extension > c_epsilon;
+}
+
+IK_QIxyzSegment::IK_QIxyzSegment()
+	: IK_QSegment(R_xyz, 3)
+	, m_weight{(Real)1, (Real)1, (Real)1}
+	, m_axis {Eigen::Vector3r::UnitX(), Eigen::Vector3r::UnitY(), Eigen::Vector3r::UnitZ()}
+	, m_locked {false, false, false}
+{
+}
+
+Real IK_QIxyzSegment::Weight(int dof_l) const
+{
+	IKAssert(-1 < dof_l && dof_l < 3);
+	return m_weight[dof_l];
+}
+
+void IK_QIxyzSegment::SetWeight(int dof_l, Real w)
+{
+	IKAssert(-1 < dof_l && dof_l < 3);
+	m_weight[dof_l] = w;
+}
+
+Eigen::Vector3r IK_QIxyzSegment::Axis(int dof_l) const
+{
+	IKAssert(-1 < dof_l && dof_l < 3);
+	return m_axis[dof_l];
+}
+
+bool IK_QIxyzSegment::Locked(int dof_l) const
+{
+	IKAssert(-1 < dof_l && dof_l < 3);
+	return m_locked[dof_l];
+}
+
+void IK_QIxyzSegment::UnLock()
+{
+	memset(m_locked, false, sizeof(m_locked));
+}
+
+void IK_QIxyzSegment::Lock(int dof_l, IK_QJacobian &jacobian, Eigen::Vector3r &delta)
+{
+	LOGIK("Lock");
+	m_locked[dof_l] = true;
+	jacobian.Lock(m_DoF_id + dof_l, delta[dof_l]);
+}
+
+void IK_QIxyzSegment::FK_Update()
+{
+	IK_QSegment::FK_Update();
+	const Transform* tm_l2w = m_bodies[0]->GetTransformLocal2World();
+	Eigen::Matrix3r linear = tm_l2w->getLinear();
+	for (int dof_l = 0; dof_l < c_num_DoF; dof_l ++)
+		m_axis[dof_l] = linear.col(dof_l);
+}
+
+bool IK_QIxyzSegment::UpdateAngle(const IK_QJacobian &jacobian, Eigen::Vector3r &delta, bool *clamp)
+{
+	return false;
+}
+
--- src/IK_QSegment.hpp
+++ src/IK_QSegment.hpp
@@ -1,4 +1,30 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * The Original Code is Copyright (C) 2001-2002 by NaN Holding BV.
+ * All rights reserved.
+ * Original author: Laurence
+ * Modified by: Wanxin Wang
+ */
+
+/** \file
+ * \ingroup iksolver
+ */
 #pragma once
+
+#include "pch.h"
 #include "IK_QJacobian.h"
 #include "ArtiBody.hpp"
 #include "macro_helper.h"
@@ -13,86 +39,95 @@ public:
 	DECLARE_ENUM_STR(Type)
 
 public:
-	bool Initialize(CArtiBodyNode* from, CArtiBodyNode* to)
-	{
-		return false;
-	}
+	IK_QSegment(Type type, int n_dof);
+	virtual ~IK_QSegment();
+	bool Initialize(CArtiBodyNode* from, CArtiBodyNode* to);
+
 	//to unify the segment positions within [0, 1], for better numerical computation performance
 	void Scale(Real s)
 	{
-
+		m_scale = s;
+		m_max_extension *= s;
 	}
+
 	// number of degrees of freedom
 	int NumberOfDoF() const
 	{
-		return 0; // joint answers this question
+    	return c_num_DoF;
 	}
 
 	// unique id for this segment, for identification in the jacobian
 	int DoFId() const
 	{
-		return 0; // joint answers this question
+    	IKAssert(m_DoF_id > -1);
+    	return m_DoF_id;
 	}
 
 	void SetDoFId(int dof_id)
 	{
-
-	}
-
-	// per dof joint weighting
-	Real Weight(int dof) const
-	{
-		return (Real)0;
-	}
-
-	// set joint weights (per axis)
-	void SetWeight(int, Real)
-	{
-	}
-
-	// update the angles using the dTheta's computed using the jacobian matrix
-	bool UpdateAngle(const IK_QJacobian &jacobian, Eigen::Vector3r &delta, bool *clamp)
-	{
-		return false; //the joint interprets dTheta
-	}
-
-	void UpdateAngleApply()
-	{
-		// does nothing, will be removed after testing
-	}
-
-	// locking (during inner clamping loop)
-	bool Locked(int dof) const
-	{
-		return false;
-	}
-
-	void Lock(int dofId, IK_QJacobian &jacobian, Eigen::Vector3r &delta)
-	{
-	}
-
-	void UnLock()
-	{
-
+    	m_DoF_id = dof_id;
 	}
 
 	// the max distance of the end of this bone from the local origin.
 	Real MaxExtension() const
 	{
-		return 0; //artciulated body answers this question
+    	return m_max_extension;
 	}
 
 	Eigen::Vector3r GlobalStart() const
 	{
-		//artciulated body answers this question
-		return Eigen::Vector3r(0, 0, 0);
+		const Transform* tm = m_bodies[c_idxFrom]->GetTransformLocal2World();
+		IKAssert( t_tr == tm->Type() );
+		return m_scale*tm->getTranslation();
 	}
 
 	Eigen::Vector3r GlobalEnd() const
 	{
 		//articulated body answers this question
-		return Eigen::Vector3r(0, 0, 0);
+		const Transform* tm = m_bodies[c_idxTo]->GetTransformLocal2World();
+		IKAssert( t_tr == tm->Type() );
+		return m_scale*tm->getTranslation();
 	}
+
+	// is a translational segment?
+	bool Translational() const
+	{
+		return false; //currently, we don't have a translational joint
+	}
+
+	// locking (during inner clamping loop)
+	virtual bool Locked(int dof) const = 0;
+
+	virtual void UnLock() = 0;
+
+	// per dof joint weighting
+	virtual Real Weight(int dof_l) const = 0;
+
+	// recursively update the global coordinates of this segment, 'global'
+ 	// is the global transformation from the parent segment
+	virtual void FK_Update()
+	{
+		CArtiBodyTree::FK_Update(m_bodies[0]);
+	}
+
+	// get axis from rotation matrix for derivative computation
+	virtual Eigen::Vector3r Axis(int dof) const = 0;
+
+	// update the angles using the dTheta's computed using the jacobian matrix
+	virtual bool UpdateAngle(const IK_QJacobian &jacobian, Eigen::Vector3r &delta, bool *clamp) = 0;
+	virtual void Lock(int dofId, IK_QJacobian &jacobian, Eigen::Vector3r &delta) = 0;
+
+	virtual void UpdateAngleApply()
+	{
+
+	}
+
+  	// set joint limits
+	virtual void SetLimit(int, double, double)
+	{
+	}
+	// set joint weights (per axis)
+	virtual void SetWeight(int dof_l, Real w) = 0;
 
 	// Eigen::Quaternionr GlobalTransform() const
 	// {
@@ -104,33 +139,41 @@ public:
 	// will be changed later
 	Eigen::Affine3r GlobalTransform() const
 	{
-		//
-		return Eigen::Affine3r::Identity();
+		const Transform* l2w = m_bodies[c_idxTo]->GetTransformLocal2World();
+		Eigen::Affine3r tm_l2w_end;
+		tm_l2w_end.linear() = l2w->getLinear();
+		tm_l2w_end.translation() = m_scale * l2w->getTranslation();
+		return tm_l2w_end;
 	}
 
-
-	Eigen::Vector3r Axis(int dof) const
-	{
-		//joint answers this question
-		return Eigen::Vector3r(0, 0, 0);
-	}
-
-	// is a translational segment?
-	bool Translational() const
-	{
-		return false; //currently, we don't have a translational joint
-	}
-
-	void FK_Update()
-	{
-
-	}
-private:
+protected:
 	Real m_scale;
+	CArtiBodyNode* m_bodies[2];
+	IJoint*	m_joints[2];
+	Real m_max_extension;
+	int m_DoF_id;
+public:
+	const Type c_type;
+	const int c_num_DoF;
+	const int c_idxFrom;
+	const int c_idxTo;
 };
 
 
 class IK_QIxyzSegment : public IK_QSegment
 {
-
+public:
+	IK_QIxyzSegment();
+	virtual Real Weight(int dof_l) const;
+	virtual void SetWeight(int dof_l, Real w);
+	virtual Eigen::Vector3r Axis(int dof_l) const;
+	virtual void FK_Update() override;
+	virtual bool UpdateAngle(const IK_QJacobian &jacobian, Eigen::Vector3r &delta, bool *clamp);
+	virtual bool Locked(int dof_l) const;
+	virtual void UnLock();
+	virtual void Lock(int dofId, IK_QJacobian &jacobian, Eigen::Vector3r &delta);
+private:
+	Real m_weight[3];
+	Eigen::Vector3r m_axis[3];
+	bool m_locked[3];
 };
