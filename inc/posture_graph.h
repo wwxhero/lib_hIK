01f9aa6fccf3bcf7790d20ac095956fddee86d7c
wanxwang <wanxin-wang@uiowa.edu>
2021-11-20 07:59:28 -0600

    psudo-code for merging PGs

--- inc/posture_graph.h
+++ inc/posture_graph.h
@@ -18,7 +18,7 @@ HIKLIB(Real, err_entry)(const _ERROR_TB* err_tb, int i_row, int i_col);
 HIKLIB(bool, dissect)(const char* confXML, const char* path_htr, const char* dir_out);
 HIKLIB(bool, trim)(const char* src, const char* dst, const char* const names_rm[], int n_names);
 HIKLIB(bool, posture_graph_gen)(const char* interests_conf_path, const char* path_htr, const char* dir_out, Real epsErr, const _ERROR_TB* err_tb);
-HIKLIB(bool, posture_graph_merge)(const char* interests_conf_path, const char* pg_dir_0, const char* pg_dir_1, const char* pg_name, Real eps_err);
+HIKLIB(HPG, posture_graph_merge)(const HPG pg_0, const HPG pg_1, const char* confXML, Real eps_err); // hg = hg_0 U hg_1
 HIKLIB(bool, convert_pg2dot)(const char* path_src, const char* path_dst);
 
 #ifdef __cplusplus
