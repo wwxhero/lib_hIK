a1f682adf6d06d2dff07bd1c10418143fd059d38
wanxwang <wanxin-wang@uiowa.edu>
2021-12-05 17:23:09 -0600

    XETB computation with GPU is done: 300 times faster

--- src/XETBUpdate_parallel.cu
+++ src/XETBUpdate_parallel.cu
@@ -17,226 +17,165 @@
  * of the programming guide with some additions like error checking.
  */
 #include "pch.h"
-#include <stdio.h>
-#include <windows.h>
-#include <iostream>
 #include <algorithm>
-
 // For the CUDA runtime routines (prefixed with "cuda_")
 #include <cuda_runtime.h>
-
 #include <helper_cuda.h>
-/**
- * CUDA Kernel Device code
- *
- * Computes the vector addition of A and B into C. The 3 vectors have the same
- * number of elements numElements.
- *      vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, numElements);
- */
-__global__ void
-vectorAdd(const float *A, const float *B, float *C, int numElements)
-{
-    int i = blockDim.x * blockIdx.x + threadIdx.x;
 
-    if (i < numElements)
-    {
-        C[i] = A[i] + B[i];
-    }
+#include "ik_logger.h"
+
+// ComputeErr_GPU<<<minGridSize, blockSize>>>(theta0_q_dev, n_theta0,
+// 												theta1_q_dev, n_theta1,
+// 											 	err_out_dev, n_err,
+// 											 	n_joints);
+
+__global__ void
+ComputeErr_GPU(const Real4* theta0_q, int n_theta0, const Real4* theta1_q, int n_theta1, Real* err_out, int64_t n_err, int n_joints, int n_threads)
+{
+	for (int i_err = blockDim.x * blockIdx.x + threadIdx.x
+		; i_err < n_err
+		; i_err += n_threads)
+	{
+		int i_theta0 = i_err / n_theta1;
+		int i_theta1 = i_err % n_theta1;
+		Real sigma_k = 0;
+		for (int i_joint = 0; i_joint < n_joints; i_joint ++)
+		{
+			auto q_0_i = theta0_q[i_theta0 * n_joints + i_joint];
+			auto q_1_i = theta1_q[i_theta1 * n_joints + i_joint];
+			auto err_k_ij = fabs( q_0_i.w * q_1_i.w
+								+ q_0_i.x * q_1_i.x
+								+ q_0_i.y * q_1_i.y
+								+ q_0_i.z * q_1_i.z);
+			sigma_k += min((Real)1.0, err_k_ij);
+		}
+		Real err_i = (Real)n_joints - sigma_k;
+		err_out[i_err] = err_i;
+	}
 }
 
 void ComputeErr(const Real4* theta0_q, int n_theta0, const Real4* theta1_q, int n_theta1, Real* err_out, int64_t n_err, int n_joints)
 {
-    for (int i_err = 0; i_err < n_err; i_err ++)
-    {
-        int i_theta0 = i_err / n_theta1;
-        int i_theta1 = i_err % n_theta1;
-        Real sigma_k = 0;
-        for (int i_joint = 0; i_joint < n_joints; i_joint ++)
-        {
-            auto q_0_i = theta0_q[i_theta0 * n_joints + i_joint];
-            auto q_1_i = theta1_q[i_theta1 * n_joints + i_joint];
-            auto err_k_ij = fabs( q_0_i.w * q_1_i.w
-                                + q_0_i.x * q_1_i.x
-                                + q_0_i.y * q_1_i.y
-                                + q_0_i.z * q_1_i.z);
-            sigma_k += std::min((Real)1.0, err_k_ij);
-        }
-        Real err_i = (Real)n_joints - sigma_k;
-        err_out[i_err] = err_i;
-    }
+#ifdef _GPU_PARALLEL
+	size_t size_theta0 = n_theta0 * n_joints * sizeof(Real4);
+	size_t size_theta1 = n_theta1 * n_joints * sizeof(Real4);
+	size_t size_err = n_err * sizeof(Real);
+
+	Real4* theta0_q_dev = NULL;
+	Real4* theta1_q_dev = NULL;
+	Real* err_out_dev = NULL;
+	cudaError_t err = cudaSuccess;
+	if (cudaSuccess != (err = cudaMalloc((void **)&theta0_q_dev, size_theta0))
+	 || cudaSuccess != (err = cudaMalloc((void **)&theta1_q_dev, size_theta1))
+	 || cudaSuccess != (err = cudaMalloc((void **)&err_out_dev, size_err)))
+	{
+		const char* errInfo = cudaGetErrorString(err);
+		LOGIKVarErr(LogInfoCharPtr, errInfo);
+		if (theta0_q_dev)
+			cudaFree(theta0_q_dev);
+		if (theta1_q_dev)
+			cudaFree(theta1_q_dev);
+		if (err_out_dev)
+			cudaFree(err_out_dev);
+		return;
+	}
+	
+	if (cudaSuccess != (err = cudaMemcpy(theta0_q_dev, theta0_q, size_theta0, cudaMemcpyHostToDevice))
+	 || cudaSuccess != (err = cudaMemcpy(theta1_q_dev, theta1_q, size_theta1, cudaMemcpyHostToDevice)))
+	{
+		const char* errInfo = cudaGetErrorString(err);
+		LOGIKVarErr(LogInfoCharPtr, errInfo);
+		if (theta0_q_dev)
+			cudaFree(theta0_q_dev);
+		if (theta1_q_dev)
+			cudaFree(theta1_q_dev);
+		if (err_out_dev)
+			cudaFree(err_out_dev);		
+		return;
+	}
+
+	int minGridSize = 0;
+	int blockSize = 0;
+	if (cudaSuccess != (err = cudaOccupancyMaxPotentialBlockSize(&minGridSize
+																, &blockSize
+																, (void*)ComputeErr_GPU)))
+	{
+		const char* errInfo = cudaGetErrorString(err);
+		LOGIKVarErr(LogInfoCharPtr, errInfo);
+		if (theta0_q_dev)
+			cudaFree(theta0_q_dev);
+		if (theta1_q_dev)
+			cudaFree(theta1_q_dev);
+		if (err_out_dev)
+			cudaFree(err_out_dev);		
+		return;
+	}
+
+	int n_threads = minGridSize * blockSize;
+	LOGIKVarErr(LogInfoInt, minGridSize);
+	LOGIKVarErr(LogInfoInt, blockSize);
+
+	ComputeErr_GPU<<<minGridSize, blockSize>>>(theta0_q_dev, n_theta0,
+												theta1_q_dev, n_theta1,
+											 	err_out_dev, n_err,
+											 	n_joints, n_threads);
+	
+	if (cudaSuccess != (err = cudaGetLastError()))
+	{
+		const char* errInfo = cudaGetErrorString(err);
+		LOGIKVarErr(LogInfoCharPtr, errInfo);
+		if (theta0_q_dev)
+			cudaFree(theta0_q_dev);
+		if (theta1_q_dev)
+			cudaFree(theta1_q_dev);
+		if (err_out_dev)
+			cudaFree(err_out_dev);
+		return;		
+	}
+	
+	if (cudaSuccess != (err = cudaMemcpy(err_out, err_out_dev, size_err, cudaMemcpyDeviceToHost)))
+	{
+		const char* errInfo = cudaGetErrorString(err);
+		LOGIKVarErr(LogInfoCharPtr, errInfo);
+		if (theta0_q_dev)
+			cudaFree(theta0_q_dev);
+		if (theta1_q_dev)
+			cudaFree(theta1_q_dev);
+		if (err_out_dev)
+			cudaFree(err_out_dev);
+		return;
+	}
+
+	if (theta0_q_dev)
+		cudaFree(theta0_q_dev);
+	if (theta1_q_dev)
+		cudaFree(theta1_q_dev);
+	if (err_out_dev)
+		cudaFree(err_out_dev);
+
+#else
+	for (int i_err = 0; i_err < n_err; i_err ++)
+	{
+		int i_theta0 = i_err / n_theta1;
+		int i_theta1 = i_err % n_theta1;
+		Real sigma_k = 0;
+		for (int i_joint = 0; i_joint < n_joints; i_joint ++)
+		{
+			auto q_0_i = theta0_q[i_theta0 * n_joints + i_joint];
+			auto q_1_i = theta1_q[i_theta1 * n_joints + i_joint];
+			auto err_k_ij = fabs( q_0_i.w * q_1_i.w
+								+ q_0_i.x * q_1_i.x
+								+ q_0_i.y * q_1_i.y
+								+ q_0_i.z * q_1_i.z);
+			sigma_k += std::min((Real)1.0, err_k_ij);
+		}
+		Real err_i = (Real)n_joints - sigma_k;
+		err_out[i_err] = err_i;
+	}
+#endif
 }
 
-/**
- * Host main routine
- */
-int
-test(void)
-{
-    // Error code to check return values for CUDA calls
-    cudaError_t err = cudaSuccess;
 
-    // Print the vector length to be used, and compute its size
-    int numElements = 50000000;
-    size_t size = numElements * sizeof(float);
-    printf("[Vector addition of %d elements]\n", numElements);
-
-    // Allocate the host input vector A
-    float *h_A = (float *)malloc(size);
-
-    // Allocate the host input vector B
-    float *h_B = (float *)malloc(size);
-
-    // Allocate the host output vector C
-    float *h_C = (float *)malloc(size);
-
-    // Verify that allocations succeeded
-    if (h_A == NULL || h_B == NULL || h_C == NULL)
-    {
-        fprintf(stderr, "Failed to allocate host vectors!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    // Initialize the host input vectors
-    for (int i = 0; i < numElements; ++i)
-    {
-        h_A[i] = rand()/(float)RAND_MAX;
-        h_B[i] = rand()/(float)RAND_MAX;
-    }
-
-    ULONGLONG tick_start = GetTickCount64();
-
-    // Allocate the device input vector A
-    float *d_A = NULL;
-    err = cudaMalloc((void **)&d_A, size);
-
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to allocate device vector A (error code %s)!\n", cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-
-    // Allocate the device input vector B
-    float *d_B = NULL;
-    err = cudaMalloc((void **)&d_B, size);
-
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to allocate device vector B (error code %s)!\n", cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-
-    // Allocate the device output vector C
-    float *d_C = NULL;
-    err = cudaMalloc((void **)&d_C, size);
-
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to allocate device vector C (error code %s)!\n", cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-
-    // Copy the host input vectors A and B in host memory to the device input vectors in
-    // device memory
-    printf("Copy input data from the host memory to the CUDA device\n");
-    err = cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
-
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to copy vector A from host to device (error code %s)!\n", cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-
-    err = cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
-
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to copy vector B from host to device (error code %s)!\n", cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-
-    // Launch the Vector Add CUDA Kernel
-    int threadsPerBlock = 256;
-    int blocksPerGrid =(numElements + threadsPerBlock - 1) / threadsPerBlock;
-    // i.e. (blocksPerGrid * threadsPerBlock - numElements) in [0, threadsPerBlock)
-    printf("CUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
-    vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, numElements);
-    err = cudaGetLastError();
-
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to launch vectorAdd kernel (error code %s)!\n", cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-
-    // Copy the device result vector in device memory to the host result vector
-    // in host memory.
-    printf("Copy output data from the CUDA device to the host memory\n");
-    err = cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
-
-    ULONGLONG tick = GetTickCount64() - tick_start;
-    // std::cout << "CPU computation cost: " << tick << " milliseconds!!!" << std::endl;
-    printf("GPU computation cost: %lld milliseconds!!!\n", tick);
-
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to copy vector C from device to host (error code %s)!\n", cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-
-    // Verify that the result vector is correct
-    for (int i = 0; i < numElements; ++i)
-    {
-        if (fabs(h_A[i] + h_B[i] - h_C[i]) > 1e-5)
-        {
-            fprintf(stderr, "Result verification failed at element %d!\n", i);
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    printf("Test PASSED\n");
-
-    tick_start = GetTickCount64();
-    for (int i = 0; i < numElements; ++i)
-    {
-        h_C[i] = (h_A[i] + h_B[i]);
-    }
-    tick = GetTickCount64() - tick_start;
-    // std::cout << "CPU computation cost: " << tick << " milliseconds!!!" << std::endl;
-    printf("CPU computation cost: %lld milliseconds!!!\n", tick);
-
-    // Free device global memory
-    err = cudaFree(d_A);
-
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to free device vector A (error code %s)!\n", cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-
-    err = cudaFree(d_B);
-
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to free device vector B (error code %s)!\n", cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-
-    err = cudaFree(d_C);
-
-    if (err != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to free device vector C (error code %s)!\n", cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-
-    // Free host memory
-    free(h_A);
-    free(h_B);
-    free(h_C);
-
-    printf("Done\n");
-    return 0;
-}
 
 
 
