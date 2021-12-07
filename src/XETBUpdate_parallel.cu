8ef371ef49fc785e0745bc5b562a5207dd843c32
wanxwang <wanxin-wang@uiowa.edu>
2021-12-06 20:58:36 -0600

    ready to test GPU code

--- src/XETBUpdate_parallel.cu
+++ src/XETBUpdate_parallel.cu
@@ -21,29 +21,69 @@
 
 #include "ik_logger.h"
 
-#define RETURN_IF_F(condition)\
-	if (condition)\
-	{\
-		const char* errInfo = cudaGetErrorString(err);\
-		LOGIKVarErr(LogInfoCharPtr, errInfo);\
-		if (theta0_q_dev)\
-			cudaFree(theta0_q_dev);\
-		if (theta1_q_dev)\
-			cudaFree(theta1_q_dev);\
-		if (err_out_dev)\
-			cudaFree(err_out_dev);\
-		return;\
-	}
+// ComputeHErr_GPU<<<minGridSize, blockSize>>>(theta_q_dev, size_theta,
+// 										 	err_out_dev, n_errs,
+// 										 	n_joints, n_threads)
 
-// ComputeErr_GPU<<<minGridSize, blockSize>>>(theta0_q_dev, n_theta0,
+__global__ void
+ComputeHErr_GPU(const Real4* theta_q, int n_theta, Real* err_out, int64_t n_err, int n_joints, int n_threads)
+{
+	for (int64_t i_err = blockDim.x * blockIdx.x + threadIdx.x
+		; i_err < n_err
+		; i_err += n_threads)
+	{
+		int64_t i_err2 = (i_err << 1);
+		int i_theta1 = (int)floor(sqrtf(i_err2));
+		int i_theta0 = i_theta1 + 1;		
+		int i_theta = 0;
+		
+		if (!(i_err2 < i_theta1 * (i_theta1 + 1)))
+			i_theta = i_theta0;
+		else
+		{
+			i_theta = i_theta1;
+			// IKAssert(i_theta * (i_theta - 1) <= i_err2);
+		}	
+
+		int j_theta2 = (int)(i_err2 - (int64_t)i_theta*(int64_t)(i_theta - 1));
+		// IKAssert(0 == (j_theta2&0x01));
+		int j_theta = (j_theta2 >> 1);
+		// IKAssert(i_theta < n_theta); 
+		// IKAssert(j_theta < n_theta);
+		// IKAssert(j_theta < i_theta);
+		// if (!(j_theta < i_theta))
+		// {
+		// 	LOGIKVarErr(LogInfoInt, i_theta);
+		// 	LOGIKVarErr(LogInfoInt, j_theta);
+		// }
+		Real sigma_i_joint = 0;
+		int i_theta_q_base = i_theta * n_joints;
+		int j_theta_q_base = j_theta * n_joints;
+		for (int i_joint = 0; i_joint < n_joints; i_joint ++)
+		{
+			auto q_i = theta_q[i_theta_q_base + i_joint];
+			auto q_j = theta_q[j_theta_q_base + i_joint];
+			auto err_k_ij = fabs( q_i.w * q_j.w
+								+ q_i.x * q_j.x
+								+ q_i.y * q_j.y
+								+ q_i.z * q_j.z);
+			sigma_i_joint += min((Real)1.0, err_k_ij);
+		}
+		Real err_i = (Real)n_joints - sigma_i_joint;
+		err_out[i_err] = err_i;
+	}
+}
+
+
+// ComputeXErr_GPU<<<minGridSize, blockSize>>>(theta0_q_dev, n_theta0,
 // 												theta1_q_dev, n_theta1,
 // 											 	err_out_dev, n_err,
 // 											 	n_joints);
 
 __global__ void
-ComputeErr_GPU(const Real4* theta0_q, int n_theta0, const Real4* theta1_q, int n_theta1, Real* err_out, int64_t n_err, int n_joints, int n_threads)
+ComputeXErr_GPU(const Real4* theta0_q, int n_theta0, const Real4* theta1_q, int n_theta1, Real* err_out, int64_t n_err, int n_joints, int n_threads)
 {
-	for (int i_err = blockDim.x * blockIdx.x + threadIdx.x
+	for (int64_t i_err = blockDim.x * blockIdx.x + threadIdx.x
 		; i_err < n_err
 		; i_err += n_threads)
 	{
@@ -70,6 +110,22 @@ ComputeErr_GPU(const Real4* theta0_q, int n_theta0, const Real4* theta1_q, int n
 void ComputeXErr(const Real4* theta0_q, int n_theta0, const Real4* theta1_q, int n_theta1, Real* err_out, int64_t n_errs, int n_joints)
 {
 #if defined _GPU_PARALLEL
+
+	#define RETURN_IF_F(condition)\
+		if (condition)\
+		{\
+			const char* errInfo = cudaGetErrorString(err);\
+			LOGIKVarErr(LogInfoCharPtr, errInfo);\
+			if (theta0_q_dev)\
+				cudaFree(theta0_q_dev);\
+			if (theta1_q_dev)\
+				cudaFree(theta1_q_dev);\
+			if (err_out_dev)\
+				cudaFree(err_out_dev);\
+			return;\
+		}
+
+
 	size_t size_theta0 = n_theta0 * n_joints * sizeof(Real4);
 	size_t size_theta1 = n_theta1 * n_joints * sizeof(Real4);
 	size_t size_err = n_errs * sizeof(Real);
@@ -91,13 +147,13 @@ void ComputeXErr(const Real4* theta0_q, int n_theta0, const Real4* theta1_q, int
 	int blockSize = 0;
 	RETURN_IF_F(cudaSuccess != (err = cudaOccupancyMaxPotentialBlockSize(&minGridSize
 																		, &blockSize
-																		, (void*)ComputeErr_GPU)));
+																		, (void*)ComputeXErr_GPU)));
 
 	int n_threads = minGridSize * blockSize;
 	// LOGIKVarErr(LogInfoInt, minGridSize);
 	// LOGIKVarErr(LogInfoInt, blockSize);
 
-	ComputeErr_GPU<<<minGridSize, blockSize>>>(theta0_q_dev, n_theta0,
+	ComputeXErr_GPU<<<minGridSize, blockSize>>>(theta0_q_dev, n_theta0,
 												theta1_q_dev, n_theta1,
 											 	err_out_dev, n_errs,
 											 	n_joints, n_threads);
@@ -112,6 +168,8 @@ void ComputeXErr(const Real4* theta0_q, int n_theta0, const Real4* theta1_q, int
 		cudaFree(theta1_q_dev);
 	if (err_out_dev)
 		cudaFree(err_out_dev);
+
+	#undef RETURN_IF_F
 
 #else
 	for (int i_err = 0; i_err < n_errs; i_err ++)
@@ -138,10 +196,58 @@ void ComputeXErr(const Real4* theta0_q, int n_theta0, const Real4* theta1_q, int
 }
 
 
-void ComputeHErr(const Real4* theta, int n_theta, Real* err_out, int64_t n_errs, int n_joints)
+void ComputeHErr(const Real4* theta_q, int n_theta, Real* err_out, int64_t n_errs, int n_joints)
 {
 #if defined _GPU_PARALLEL
 
+	#define RETURN_IF_F(condition)\
+		if (condition)\
+		{\
+			const char* errInfo = cudaGetErrorString(err);\
+			LOGIKVarErr(LogInfoCharPtr, errInfo);\
+			if (theta_q_dev)\
+				cudaFree(theta_q_dev);\
+			if (err_out_dev)\
+				cudaFree(err_out_dev);\
+			return;\
+		}
+
+	size_t size_theta = n_theta * n_joints * sizeof(Real4);
+	size_t size_err = n_errs * sizeof(Real);
+
+	Real4* theta_q_dev = NULL;
+	Real* err_out_dev = NULL;
+	cudaError_t err = cudaSuccess;
+
+	RETURN_IF_F(cudaSuccess != (err = cudaMalloc((void **)&theta_q_dev, size_theta))
+	 			|| cudaSuccess != (err = cudaMalloc((void **)&err_out_dev, size_err)));
+	
+	RETURN_IF_F(cudaSuccess != (err = cudaMemcpy(theta_q_dev, theta_q, size_theta, cudaMemcpyHostToDevice)));
+	 			
+	int minGridSize = 0;
+	int blockSize = 0;
+	RETURN_IF_F(cudaSuccess != (err = cudaOccupancyMaxPotentialBlockSize(&minGridSize
+																		, &blockSize
+																		, (void*)ComputeHErr_GPU)));
+
+	int n_threads = minGridSize * blockSize;
+	// LOGIKVarErr(LogInfoInt, minGridSize);
+	// LOGIKVarErr(LogInfoInt, blockSize);
+
+	ComputeHErr_GPU<<<minGridSize, blockSize>>>(theta_q_dev, n_theta,
+											 	err_out_dev, n_errs,
+											 	n_joints, n_threads);
+	
+	RETURN_IF_F(cudaSuccess != (err = cudaGetLastError()));
+	
+	RETURN_IF_F(cudaSuccess != (err = cudaMemcpy(err_out, err_out_dev, size_err, cudaMemcpyDeviceToHost)));
+
+	if (theta_q_dev)
+		cudaFree(theta_q_dev);
+	if (err_out_dev)
+		cudaFree(err_out_dev);
+
+	#undef RETURN_IF_F
 #else
 	for (int64_t i_err = 0; i_err < n_errs; i_err ++)
 	{
@@ -174,8 +280,8 @@ void ComputeHErr(const Real4* theta, int n_theta, Real* err_out, int64_t n_errs,
 		int j_theta_q_base = j_theta * n_joints;
 		for (int i_joint = 0; i_joint < n_joints; i_joint ++)
 		{
-			auto q_i = theta[i_theta_q_base + i_joint];
-			auto q_j = theta[j_theta_q_base + i_joint];
+			auto q_i = theta_q[i_theta_q_base + i_joint];
+			auto q_j = theta_q[j_theta_q_base + i_joint];
 			auto err_k_ij = fabs( q_i.w * q_j.w
 								+ q_i.x * q_j.x
 								+ q_i.y * q_j.y
@@ -187,8 +293,3 @@ void ComputeHErr(const Real4* theta, int n_theta, Real* err_out, int64_t n_errs,
 	}
 #endif
 }
-
-
-
-
-#undef RETURN_IF_F
